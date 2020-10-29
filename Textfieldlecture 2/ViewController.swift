219dd14ee6f5c8d2f1b1dc27f9d22497f0385c78
Jandrija Krneta <Jkrneta1181@stu.d214>
2020-10-28 22:33:58 -0700

    Added code to add the textfield together

--- Textfieldlecture 2/ViewController.swift
+++ Textfieldlecture 2/ViewController.swift
@@ -16,19 +16,29 @@ class ViewController: UIViewController {
     let textfield2 = UITextField(frame: CGRect(x: 20, y: 100, width: 100, height: 34))
     
     override func viewDidLoad() {
-        
+         super.viewDidLoad()
         textfield2.borderStyle = .roundedRect
         
         view.addSubview(textfield2)
         
+    }
+        
+    @IBAction func buttonWasPressed(_ sender: Any){
+       
+        let number1String = textfield1.text!
+        let number2String = textfield2.text!
         
         
-        @IBAction func buttonWasPressed(_ sender: Any ) {
+        let number1 = Int(number1String)!
+        let number2 = Int(number2String)!
+        
+        let answer = number1 + number2
+        
+        outputLabel.text = String(answer)
             
         }
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+      
+    
 
 
 }
